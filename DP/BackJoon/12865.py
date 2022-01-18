c3cf3d69182fc14df5e7d01ae32dbf39b6e8acd6
sala0320 <sala0320@naver.com>
2022-01-18 15:36:14 +0900

    22.01.18

--- DP/BackJoon/12865.py
+++ DP/BackJoon/12865.py
@@ -1,4 +1,12 @@
-#무게가 작은 순으로 넣기, 가치가 큰순으로 넣기 안됨
-
+#dp[i][j] = max(dp[i-1][j], dp[i-1][j - W[i]] + V[i])
 N, K = map(int, input().split())
+#W, V
 pack = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        if j - pack[i-1][0] >= 0:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-pack[i-1][0]] + pack[i-1][1])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[N][K])
