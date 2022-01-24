f88b4c56c40468f40fb9c6c54a5a089479c7b750
HaewonCho <sala0320@naver.com>
2022-01-24 15:28:45 +0900

    22.01.24

--- BFS+DFS/BackJoon/19238.py
+++ BFS+DFS/BackJoon/19238.py
@@ -12,24 +12,27 @@ N, M, F = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 X, Y = map(int, input().split())
 destination = []
-for m in range(1, M+1):
+for m in range(1, M + 1):
     info = list(map(int, input().split()))
-    board[info[0]-1][info[1]-1] = -m
-    destination.append((info[2]-1, info[3]-1))
+    board[info[0] - 1][info[1] - 1] = -m
+    destination.append((info[2] - 1, info[3] - 1))
 
-def find_passenger(x,y,f,c):
+
+def find_passenger(x, y, f, c):
     return
 
-def find_destination(x,y,f,c):
+
+def find_destination(x, y, f, c):
     return
+
 
 for _ in range(M):
-    X, Y, F, C = find_passenger(X,Y,F,0)
+    X, Y, F, C = find_passenger(X, Y, F, 0)
     if X == -1:
         print(-1)
         break
     else:
-        X, Y, F, C = find_destination(X,Y,F,0)
+        X, Y, F, C = find_destination(X, Y, F, 0)
         if X == -1:
             print(-1)
             break
